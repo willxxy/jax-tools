185229d0f575cc8eb014bbbdd657fcc2b41dbaef
willxxy <90741489+willxxy@users.noreply.github.com>
2024-10-14 01:47:58 -0400

    Update utility_fns.py

--- utility_fns.py
+++ utility_fns.py
@@ -17,11 +17,6 @@ def roulette_jax(key, pArr):
     choice = jnp.sum(spin > cumsum)
     return jnp.where(choice < len(pArr), choice + 1, len(pArr))
 
-def listXor(b,c):
-  """Returns elements in lists b and c they don't share
-  """
-  A = [a for a in b+c if (a not in b) or (a not in c)]
-  return A
 
 @jax.jit
 def rankArray_jax(X):
@@ -30,25 +25,6 @@ def rankArray_jax(X):
     """
     tmp = jnp.argsort(X)
     return jnp.argsort(tmp)
-
-def tiedRank(X):  
-  """Returns ranking of a list, with ties recieving and averaged rank
-  # Modified from: github.com/cmoscardi/ox_ml_practical/blob/master/util.py
-  """
-  Z = [(x, i) for i, x in enumerate(X)]  
-  Z.sort(reverse=True)  
-  n = len(Z)  
-  Rx = [0]*n   
-  start = 0 # starting mark  
-  for i in range(1, n):  
-     if Z[i][0] != Z[i-1][0]:
-       for j in range(start, i):  
-         Rx[Z[j][1]] = float(start+1+i)/2.0;
-       start = i
-  for j in range(start, n):  
-    Rx[Z[j][1]] = float(start+1+n)/2.0;
-
-  return jnp.asarray(Rx)
 
 @jax.jit
 def bestIntSplit_jax(ratio, total):
@@ -75,17 +51,3 @@ def bestIntSplit_jax(ratio, total):
     intSplit = intSplit + update_mask[deserving]
     
     return intSplit
-
-def quickINTersect(A, B):
-    """ Faster set intersect: only valid for vectors of positive integers.
-    (useful for matching indices)
-    """
-    if (len(A) == 0) or (len(B) == 0):
-        return [], []
-    P = np.zeros((1+max(max(A),max(B))), dtype=bool)
-    P[A] = True
-    IB = P[B]
-    P[A] = False  # Reset
-    P[B] = True
-    IA = P[A]
-    return IA, IB
